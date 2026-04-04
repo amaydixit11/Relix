eb445e371803563475b6e5b5d6c48761cbe70e66
Amay Dixit <amaydixit11@gmail.com>
2026-04-04 16:38:15 +0530

    chore: package and exports cleanup

--- packages/core/src/services/index.ts
+++ packages/core/src/services/index.ts
@@ -1,5 +1,4 @@
 export { NoteService, noteService } from './NoteService';
 export { FileService, fileService } from './FileService';
 export { GraphService, graphService } from './GraphService';
-export { VersionService, versionService } from './VersionService';
 export { ExportService, exportService } from './ExportService';
